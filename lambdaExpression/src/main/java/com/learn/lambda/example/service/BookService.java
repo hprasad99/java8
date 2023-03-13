cfc3a8dd943e72b6ed42b97536da89080f3267c2
hprasad99 <himanshu.prasad@ril.com>
2023-03-13 21:02:30 +0530

    basic lambda implementation

--- lambdaExpression/src/main/java/com/learn/lambda/example/service/BookService.java
+++ lambdaExpression/src/main/java/com/learn/lambda/example/service/BookService.java
@@ -4,14 +4,13 @@ import com.learn.lambda.example.dao.BookDAO;
 import com.learn.lambda.example.entity.Book;
 
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class BookService {
 
     public List<Book> getBooksinSort() {
         List<Book> books = new BookDAO().getBooks();
-        Collections.sort(books, new MyComparator());
+        Collections.sort(books, (o1, o2) -> o1.getName().compareTo(o2.getName()));
         return books;
     }
     public static void main(String[] args) {
@@ -19,10 +18,10 @@ public class BookService {
     }
 }
 
-class MyComparator implements Comparator<Book> {
+/*class MyComparator implements Comparator<Book> {
 
     @Override
     public int compare(Book o1, Book o2) {
         return o2.getName().compareTo(o1.getName());
     }
-}
+}*/
